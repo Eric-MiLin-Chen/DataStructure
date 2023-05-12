df705dea505b85d16f3ea5542f3c92635b09c46b
EnTeR-uSeRnAmEs <2479880643@qq.com>
2023-05-12 21:45:05 +0800

    Formate update

--- Week4/Src/Py/DataGenerator.py
+++ Week4/Src/Py/DataGenerator.py
@@ -6,19 +6,17 @@ import sys
 def generate_sorted_data(size: int):
     return list(np.arange(size))
 
+
 # 生成逆序数据
-
-
 def generate_reverse_data(size: int):
     return list(np.arange(size, 0, -1))
 
+
 # 生成基本有序数据
-
-
 def generate_partially_sorted_data(size: int):
     data = np.arange(size)
     # 随机打乱部分数据
-    np.random.shuffle(data[:int(size * 0.1)])
+    np.random.shuffle(data[: int(size * 0.1)])
     return list(data)
 
 
@@ -31,15 +29,17 @@ def generate_random_data(size: int):
 def SaveData(filename: str, data: list[int]):
     src_path = sys.path[0]
     relative_path = "../../Data"
-    with open("/".join([src_path, relative_path, filename + str(len(data))+".txt"]), 'w') as file:
+    with open(
+        "/".join([src_path, relative_path, filename + str(len(data)) + ".txt"]), "w"
+    ) as file:
         file.write(" ".join(map(str, data)))
 
 
 # 调用函数生成数据
 size = 32
 for i in range(9):
-    SaveData('SortedData', generate_sorted_data(size))
-    SaveData('ReversedData', generate_reverse_data(size))
-    SaveData('PartiallySortedData', generate_partially_sorted_data(size))
-    SaveData('RandomData', generate_random_data(size))
+    SaveData("SortedData", generate_sorted_data(size))
+    SaveData("ReversedData", generate_reverse_data(size))
+    SaveData("PartiallySortedData", generate_partially_sorted_data(size))
+    SaveData("RandomData", generate_random_data(size))
     size *= 2
