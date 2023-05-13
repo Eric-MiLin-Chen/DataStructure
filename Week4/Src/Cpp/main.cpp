d2cc6359cfc719b980cf1cc6f6c5ec4100e6c15c
EnTeR-uSeRnAmEs <2479880643@qq.com>
2023-05-13 09:34:58 +0800

    update SaveData()

--- Week4/Src/Cpp/main.cpp
+++ Week4/Src/Cpp/main.cpp
@@ -38,12 +38,50 @@ Ans TestFunc(Ans (*func)(int arr[], int n), const int arr[], int n)
 vector<int> LoadData(const string FileName)
 {
     ifstream file("./../../Data/" + FileName);
-    // cout << FileName << endl;
     vector<int> Data;
     int tmp;
     while (file >> tmp)
         Data.push_back(tmp);
     return Data;
+}
+
+void Display(long long (*result)[4][9][2])
+{
+    for (int f = 0; f < 8; f++)
+    {
+        cout << "Function " << f + 1 << ": " << endl;
+        for (int type = 0; type < 4; type++)
+        {
+            cout << "    File Type " << type + 1 << endl;
+            for (int dtype = 0; dtype < 2; dtype++)
+            {
+                cout << "    ";
+                for (int size = 0; size < 9; size++)
+                    cout << result[f][type][size][dtype] << " ";
+                cout << endl;
+            }
+        }
+    }
+}
+
+void SaveData(long long (*result)[4][9][2])
+{
+    ofstream file;
+    file.open("./../../Data/File.txt", ios::out);
+    for (int f = 0; f < 8; f++)
+    {
+        for (int type = 0; type < 4; type++)
+        {
+            for (int dtype = 0; dtype < 2; dtype++)
+            {
+                for (int size = 0; size < 9; size++)
+                {
+                    file << result[f][type][size][dtype] << " ";
+                }
+                file << "\n";
+            }
+        }
+    }
 }
 
 int main()
@@ -52,43 +90,27 @@ int main()
     string TestFileType[4] = {"PartiallySortedData", "RandomData", "ReversedData", "SortedData"};
     string TestFileSize[9] = {{"32"}, {"64"}, {"128"}, {"256"}, {"512"}, {"1024"}, {"2048"}, {"4096"}, {"8192"}};
     Ans (*func[8])(int arr[], int n) = {insertionSort, shellSort, bubbleSort, quickSort, selectionSort, heapSort, mergeSort, radixSort};
-    // vector<int> test = LoadData(TestFileType[3] + TestFileSize[0] + ".txt");
-    // for (int i = 0; i < 32; i++)
-    // {
-    //     cout << test[i] << " ";
-    // }
-    for (int f = 0; f < 8; f++)
+
+    for (int type = 0; type < 4; type++)
     {
-        for (int type = 0; type < 4; type++)
+        for (int size = 0; size < 9; size++)
         {
-            for (int size = 0; size < 9; size++)
+            vector<int> vData = LoadData(TestFileType[type] + TestFileSize[size] + ".txt");
+            int Data[vData.size()];
+            for (int i = 0; i < vData.size(); i++)
+                Data[i] = vData[i];
+            for (int f = 0; f < 8; f++)
             {
-                vector<int> vData = LoadData(TestFileType[type] + TestFileSize[size] + ".txt");
-                int Data[vData.size()];
-                for (int i = 0; i < vData.size(); i++)
-                    Data[i] = vData[i];
                 Ans tmp = TestFunc(func[f], Data, vData.size());
                 result[f][type][size][0] = tmp.CompareCount;
                 result[f][type][size][1] = tmp.MoveCount;
             }
         }
     }
-    for (int f = 0; f < 8; f++)
-    {
-        cout << "Function " << f + 1 << ": " << endl;
-        for (int type = 0; type < 4; type++)
-        {
-            cout << "    File Type: " << TestFileType[type] << endl;
-            for (int dtype = 0; dtype < 2; dtype++)
-            {
-                cout << "    ";
-                for (int size = 0; size < 9; size++)
-                {
-                    cout << result[f][type][size][dtype] << " ";
-                }
-                cout << endl;
-            }
-        }
-    }
+
+    Display(result);
+
+    SaveData(result);
+
     return 0;
 }
